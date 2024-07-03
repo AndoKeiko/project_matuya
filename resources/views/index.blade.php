a82b1f58439605af196d2dc36c3a9bcb1e5f2f24
mike <re0027iee@gmail.com>
2024-07-03 12:22:38 +0900

    ボタン修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="flex justify-center space-x-4 py-12">
+<div class="flex justify-center space-x-8 py-12">
     <a href="{{ route('category.page') }}" class="btn btn-dinein">店舗</a>
     <a href="{{ route('takeaway.page') }}" class="btn btn-secondary">お持ち帰り</a>
 </div>
