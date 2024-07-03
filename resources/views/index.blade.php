6ad3cbba4ea5e319daec73ee1770bc3f05aac3ed
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-07-03 14:20:01 +0900

    css修正6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="flex justify-center space-x-4 py-12">
-    <a href="{{ route('category') }}" class="btn btn-dinein">店舗</a>
+    <a href="" class="btn btn-dinein">店舗</a>
     <a href="" class="btn btn-secondary">お持ち帰り</a>
 </div>
 @endsection
