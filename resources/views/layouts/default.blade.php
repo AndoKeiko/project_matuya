ec7c05ad269c080a70b0a52a1b953d505350c4cb
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-07-03 13:59:57 +0900

    css追加3

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,7 +12,7 @@
   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
   <link href="{{asset('/css/style.css')}}" rel="stylesheet">
   {{--@vite('resources/css/app.css')--}}
-  <link href="{{asset('/css/style/app-DB854DD1.css')}}" rel="stylesheet">
+  <link href="{{asset('/css/style/app.css')}}" rel="stylesheet">
   <!-- Scripts -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 </head>
